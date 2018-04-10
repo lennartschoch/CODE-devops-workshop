10cf2af5929f78dab4c8f926058a8533bdfa0ef6
Lennart Schoch <lennart.schoch@code.berlin>
2018-04-10 12:04:25 +0200

    Made test fail

--- app/tests/unit/test_logic.py
+++ app/tests/unit/test_logic.py
@@ -4,7 +4,7 @@ from calculator.logic import Calculator
 
 class CalculatorTests(TestCase):
     def test_mul(self):
-        assert Calculator().mul(100, 10) == 1000
+        assert Calculator().mul(100, 10) == 2000
 
     def test_div(self):
         pass
