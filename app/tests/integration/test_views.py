ea4ea46342e939bda751823a901d5c3ecd73bc55
Lennart Schoch <lennart.schoch@code.berlin>
2018-04-10 12:10:51 +0200

    Enable integration test

--- app/tests/integration/test_views.py
+++ app/tests/integration/test_views.py
@@ -9,7 +9,6 @@ class ViewTests(TestCase):
         self.client = TestClient(app)
 
     def test_multiply(self):
-        # r = self.client.get("/calc/3*10")
-        # self.assertEquals(r.status_code, 200)
-        # self.assertEquals(r.body, "30")
-        pass
+        r = self.client.get("/calc/3*10")
+        self.assertEquals(r.status_code, 200)
+        self.assertEquals(r.body, "30")
